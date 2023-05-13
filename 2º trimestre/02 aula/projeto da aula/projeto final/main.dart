d2602b6116c15806a5d92281c8944733c0f79978
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2023-05-13 12:54:58 -0300

    Delete 2º trimestre/02 aula/projeto da aula/projeto final directory

--- 2º trimestre/02 aula/projeto da aula/projeto final/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:persistencia/app.dart';
-
-void main() {
-  runApp(const App());
-}
--- 2º trimestre/02 aula/projeto da aula/projeto final/rota.dart
+++
@@ -1,5 +0,0 @@
-class Rota{
-  static const String contatoLista = '/';
-  static const String contatoForm = 'contato_form';
-  static const String contatoDetalhe = 'contato_detalhe';
-}
