3cab320ecad59b93e0ef62493588068a4615444e
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2023-05-15 19:40:31 -0300

    Add files via upload

---
+++ 2º trimestre/02 aula/projeto da aula/projeto final/lib/view/contato_lista.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:persistencia/database/daofake/contato_dao_fake.dart';
+import 'package:persistencia/rota.dart';
+import 'package:persistencia/view/dto/contato.dart';
+import 'package:persistencia/view/interface/contato_interface_dao.dart';
+import 'package:persistencia/view/widget/barra_navegacao.dart';
+import 'package:persistencia/view/widget/botao_adicionar.dart';
+import 'package:persistencia/view/widget/foto_contato.dart';
+import 'package:persistencia/view/widget/painel_botoes.dart';
+
+class ContatoLista extends StatelessWidget {
+  ContatoLista({Key? key}) : super(key: key);
+  ContatoInterfaceDAO dao = ContatoDAOFake();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Lista Contatos')),
+      body: criarLista(context),
+      floatingActionButton: BotaoAdicionar(acao: ()=>Navigator.pushNamed(context, Rota.contatoForm)),
+      bottomNavigationBar: const BarraNavegacao(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked
+    );
+  }
+
+  Widget criarLista(BuildContext context) {
+    return FutureBuilder(
+      future: dao.consultarTodos(),
+      builder: (context,AsyncSnapshot<List<Contato>> lista){
+        if(!lista.hasData) return const CircularProgressIndicator();
+        if(lista.data == null) return const Text('Não há contatos...');
+        List<Contato> listaContatos = lista.data!;
+        return ListView.builder(
+          itemCount: listaContatos.length,
+          itemBuilder: (context, indice) {
+            var contato = listaContatos[indice];
+            return criarItemLista(context, contato);
+          },
+        );
+      },
+    );
+  }
+
+  Widget criarItemLista(BuildContext context, Contato contato){
+    return ItemLista(
+      contato: contato, 
+      alterar: () {
+        Navigator.pushNamed(context, Rota.contatoForm); 
+      },
+      detalhes: (){
+        Navigator.pushNamed(context, Rota.contatoDetalhe);
+      }, 
+      excluir: (){
+        dao.excluir(contato.id);
+      } 
+    );
+  }
+}
+
+class ItemLista extends StatelessWidget {
+  final Contato contato;
+  final VoidCallback alterar;
+  final VoidCallback detalhes;
+  final VoidCallback excluir;
+
+  const ItemLista({required this.contato,required this.alterar, required this.detalhes, required this.excluir, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(  
+      leading: FotoContato(contato: contato),
+      title: Text(contato.nome),
+      subtitle: Text(contato.telefone),
+      trailing: PainelBotoes(
+        alterar: alterar, 
+        excluir: excluir
+      ),
+      onTap: detalhes,
+    );
+  }
+}
